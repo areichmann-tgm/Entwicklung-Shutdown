1f7e97a96eb57d291d5bcb48f8330d0d66ff119d
adrian reichmann <areichmann@student.tgm.ac.at>
2017-05-04 16:18:25 +0200

    Back Button versuch FUNKTIONIERT EH NICHT DER SCH...

---
+++ Assets/UseBackbutton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UseBackbutton : MonoBehaviour
+{
+
+    public void Back()
+    {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                SceneManager.LoadScene("start");
+
+                
+            }
+        print("Hallo");
+        
+    }
+}
