c67e38cdaa36fecf45516cf45dda4c65e7201476
adrian reichmann <areichmann@student.tgm.ac.at>
2017-05-31 21:58:05 +0200

    ok

---
+++ Assets/movingScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingScript : MonoBehaviour
+{
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary)
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+            double halfScreen = Screen.width / 2.0;
+
+            //Check if it is left or right?
+            if (touchPosition.x < halfScreen)
+            {
+                player.transform.Translate(Vector3.left * 5 * Time.deltaTime);
+            }
+            else if (touchPosition.x > halfScreen)
+            {
+                player.transform.Translate(Vector3.right * 5 * Time.deltaTime);
+            }
+
+        }
+
+    }
+
+}
